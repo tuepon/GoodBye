6db766a06eeb8f70ce57b55f9b957c501915d0e4
Tomonori Ueno <t.uepon@gmail.com>
2018-10-14 20:51:12 +0900

    first commit

---
+++ app/src/main/java/net/webisle/goodbye/GoodByeActivity.java
@@ -0,0 +1,13 @@
+package net.webisle.goodbye;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GoodByeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_good_bye);
+    }
+}
